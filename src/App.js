25e8a67b59ecee9668dcf41b884ff2705cb356be
Dalielmokhtar <50365725+Dalielmokhtar@users.noreply.github.com>
2019-05-24 12:30:10 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
-import Home from './Home'
-function App() {
-  return (
-    <div className="App">
-<Home />
+import Home from './Home';
+import WithLoading from './Loader'
 
-    </div>
-  );
+const MovieWithLoading = WithLoading(Home)
+
+export default class App extends Component {
+  state = {
+    loading :false
+  }
+ 
+
+  componentDidMount() {
+    setTimeout(() => {this.setState({
+      loading:true
+    })
+      
+    }, 3000);
+  
+    
+  }
+  render() {
+    return (
+      <MovieWithLoading isLoading={this.state.loading}/>
+    )
+  }
 }
-
-export default App;
